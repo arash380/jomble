d5b32e443279ef4f62d3f95a33a77d6793405c05
arash380 <arsa0328@gmail.com>
2022-08-03 19:14:27 -0700

    Minor axios changes and added helper functions.

--- frontend/src/services/api/authAxios.js
+++ frontend/src/services/api/authAxios.js
@@ -1,15 +1,16 @@
 import { DEFAULT_API as axios } from "./AxiosInstance";
 import { toast } from "react-toastify";
-const AUTH_PREFIX = "/auth";
+import { _catchAPIError } from "../utils/helpers";
+
+const AUTH_PREFIX = "/auth/";
 
 export const login = async (email, password) => {
   try {
-    const r = await axios.post(`${AUTH_PREFIX}/login`, { email, password });
+    const r = await axios.post(`${AUTH_PREFIX}login`, { email, password });
     toast.success("Logged in successfully!");
     return r.data.token;
   } catch (err) {
-    if (err.response.status === 401) toast.error(err.response.data);
-    else toast.error("Something went wrong!");
+    _catchAPIError(err);
   }
 
   return null;
@@ -17,12 +18,11 @@ export const login = async (email, password) => {
 
 export const createUser = async (user) => {
   try {
-    const r = await axios.post(`${AUTH_PREFIX}/register`, { ...user });
+    const r = await axios.post(`${AUTH_PREFIX}register`, { ...user });
     toast.success(`Created user for ${user.firstName} ${user.lastName}`);
     return r.data.token;
   } catch (err) {
-    if (err.response.status === 401) toast.error(err.response.data);
-    else toast.error("Something went wrong!");
+    _catchAPIError(err);
   }
 
   return null;
---
+++ frontend/src/services/utils/helpers.js
@@ -0,0 +1,18 @@
+export const fileToBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
+export const _catchAPIError = (err) => {
+  const msg = err.response.data.errors[0];
+  if (err.response.status === 400 && msg) {
+    toast.error(msg);
+  } else {
+    toast.error("Something went wrong!");
+  }
+
+  throw new Error("Failed!");
+};
