aabc33cd12c80b13ba85756ae8b15b8b442b4d3e
arash380 <arsa0328@gmail.com>
2022-08-07 19:14:03 -0700

    Styled auth layout.

--- frontend/src/components/routing/Layouts/AuthLayout/AuthLayout.js
+++ frontend/src/components/routing/Layouts/AuthLayout/AuthLayout.js
@@ -2,19 +2,19 @@ import { Outlet } from "react-router-dom";
 import classes from "./AuthLayout.module.css";
 
 const AuthLayout = () => (
-  <section className={classes.root}>
+  <div className={classes.root}>
     <div className={classes.card}>
       <div className={classes.left}>
         <Outlet />
       </div>
       <div className={classes.right}>
         <p>
-          Welcome to <b>Jomble</b>
+          Welcome to <strong>Jomble</strong>
         </p>
         <span>Something about the app here</span>
       </div>
     </div>
-  </section>
+  </div>
 );
 
 export default AuthLayout;
