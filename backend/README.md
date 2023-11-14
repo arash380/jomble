19374cdbcf10dbe28d4104483510a6c33f28fdfe
arash380 <arsa0328@gmail.com>
2023-11-13 20:58:21 -0800

    I'm back!

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# .Net WebApi
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `dotnet run`
+
+Runs the app in the development mode.\
+Open [https://localhost:7199/swagger/index.html](https://localhost:7199/swagger/index.html) to view it in your browser.
--- frontend/src/services/api/authAxios.js
+++ frontend/src/services/api/authAxios.js
@@ -2,11 +2,11 @@ import { DEFAULT_API as axios } from "./AxiosInstance";
 import { toast } from "react-toastify";
 import { _catchAPIError } from "../utils/helpers";
 
-const AUTH_PREFIX = "/auth/";
+const AUTH_PREFIX = "/auth";
 
 export const login = async (email, password) => {
   try {
-    const r = await axios.post(`${AUTH_PREFIX}login`, { email, password });
+    const r = await axios.post(`${AUTH_PREFIX}/login`, { email, password });
     toast.success("Logged in successfully!");
     return r.data.token;
   } catch (err) {
@@ -18,7 +18,7 @@ export const login = async (email, password) => {
 
 export const createUser = async (user) => {
   try {
-    const r = await axios.post(`${AUTH_PREFIX}register`, { ...user });
+    const r = await axios.post(`${AUTH_PREFIX}/register`, { ...user });
     toast.success(`Created user for ${user.firstName} ${user.lastName}`);
     return r.data.token;
   } catch (err) {
